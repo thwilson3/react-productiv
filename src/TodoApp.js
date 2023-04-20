6015b2a4d8c38469c961463f6d3e21285840bb82
William Squires <william.squiresii@gmail.com>
2023-04-20 17:38:47 -0500

    re

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -44,18 +44,18 @@ function TodoApp({ initialTodos }) {
             <span className="text-muted">You have no todos.</span>}
         </div>
 
-          <div className="col-md-6">
-        {todos.length > 0 &&
+        <div className="col-md-6">
+          {todos.length > 0 &&
             <section className="mb-4">
               <h3>Top Todo</h3>
               <TopTodo todos={todos} />
             </section>}
 
-            <section>
-              <h3 className="mb-3">Add Nü</h3>
-              <TodoForm handleSave={create} />
-            </section>
-          </div>
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            <TodoForm handleSave={create} />
+          </section>
+        </div>
       </div>
     </main>
   );
