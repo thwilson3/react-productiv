2d04f88f57bb7e7095f0f9845abe6c83e1d79fdb
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-20 16:29:41 -0500

    initial commit

---
+++ src/TodoApp.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import TopTodo from "./TopTodo";
+import EditableTodoList from "./EditableTodoList";
+
+/** App for managing a todo list.
+ *
+ * Props:
+ * - initialTodos: possible array of [ todo, ... ]
+ *
+ * State:
+ * - todos: array of [ todo, ... ]
+ *
+ * App -> TodoApp -> { TodoForm, EditableTodoList }
+ */
+
+function TodoApp({ initialTodos }) {
+  const [todos, setTodos] = useState({ initialTodos });
+
+  /** add a new todo to list */
+  function create(newTodo) {
+    setTodos((t) => [...todos, { ...newTodo, id: uuid() }]);
+  }
+
+  /** update a todo with updatedTodo */
+  function update(updatedTodo) {
+    setTodos((t) =>
+      todos.map((t) => (t.id === updatedTodo.id ? updatedTodo : t))
+    );
+  }
+
+  /** delete a todo by id */
+  function remove(id) {
+    setTodos(t => todos.filter(t => t.id !== id));
+  }
+  return (
+    <main className="TodoApp">
+      <div className="row">
+        <div className="col-md-6">
+          <EditableTodoList update={update} remove={remove}/> OR
+          <span className="text-muted">You have no todos.</span>
+        </div>
+
+        <div className="col-md-6">
+          (if no top todo, omit this whole section)
+          <section className="mb-4">
+            <h3>Top Todo</h3>
+            <TopTodo />
+          </section>
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            FIXME
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default TodoApp;
