2cde2daa53569124da1a5ffebb35a17583db5484
William Squires <william.squiresii@gmail.com>
2023-04-20 17:38:20 -0500

    updated functionality for handling no todos

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -39,21 +39,23 @@ function TodoApp({ initialTodos }) {
     <main className="TodoApp">
       <div className="row">
         <div className="col-md-6">
-          <EditableTodoList todos={todos} update={update} remove={remove}/> OR
-          <span className="text-muted">You have no todos.</span>
+          {todos.length > 0 ?
+            <EditableTodoList todos={todos} update={update} remove={remove} /> :
+            <span className="text-muted">You have no todos.</span>}
         </div>
 
-        <div className="col-md-6">
-          (if no top todo, omit this whole section)
-          <section className="mb-4">
-            <h3>Top Todo</h3>
-            <TopTodo todos={todos}/>
-          </section>
-          <section>
-            <h3 className="mb-3">Add Nü</h3>
-            <TodoForm handleSave={create}/>
-          </section>
-        </div>
+          <div className="col-md-6">
+        {todos.length > 0 &&
+            <section className="mb-4">
+              <h3>Top Todo</h3>
+              <TopTodo todos={todos} />
+            </section>}
+
+            <section>
+              <h3 className="mb-3">Add Nü</h3>
+              <TodoForm handleSave={create} />
+            </section>
+          </div>
       </div>
     </main>
   );
